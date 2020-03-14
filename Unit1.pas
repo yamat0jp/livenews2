32303a27b67c69509324827cbe4aa8b088a7106c
yamat0jp <terukohietori@gmail.com>
2020-03-14 17:49:53 +0900

    まさにここ、スタイルシートはUTF8で読まないことにしました　よくわからない

--- Unit1.pas
+++ Unit1.pas
@@ -425,7 +425,7 @@ begin
     else if (str2 = '/style') and (str <> '') then
     begin
       Zip.Read(name, stream, ziph);
-      list.LoadFromStream(stream, TEncoding.UTF8);
+      list.LoadFromStream(stream);
       stream.Free;
       image.AppendRecord([imgid, v, nid, id, str, '', list.Text, false]);
       inc(imgid);
